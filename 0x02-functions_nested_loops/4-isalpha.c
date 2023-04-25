835eaeff677781e4763b8d9663a83b838b7b0e50
wafaetaouil <wafaetaouil@gmail.com>
2023-04-25 02:15:28 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,12 +5,11 @@
  *
  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
-int _isalpha(int c);
-
+int _isalpha(int c)
 {
-	if ((c >= 'a' ; c >= 'z') || (c >= 'A' ; c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
 		return (1);
-
 	else
 		return (0);
 }
