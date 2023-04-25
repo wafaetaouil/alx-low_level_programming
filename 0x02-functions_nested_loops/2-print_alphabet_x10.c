9071160224368daf6f4e2d2421b83c9175a7c1d2
wafaetaouil <wafaetaouil@gmail.com>
2023-04-25 01:40:54 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 97 ; j <= 122 ; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
