422cea58c7d4aad2ea681a27b7f1457e0dfb8e3a
wafaetaouil <wafaetaouil@gmail.com>
2023-04-26 04:26:49 -0700

    2main

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,8 +10,8 @@
 
 int largest_number(int a, int b, int c)
 {
-       	int largest;
-       
+	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -24,6 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
