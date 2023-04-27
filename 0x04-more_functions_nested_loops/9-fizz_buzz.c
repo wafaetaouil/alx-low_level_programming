907aeda6025654ab5c29430fe6bd8d3af640d7ed
wafaetaouil <wafaetaouil@gmail.com>
2023-04-27 04:06:50 -0700

    ./9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main -  checks for checks for a digit (0 through 9).
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			if (x % 3 == 0)
+			{
+				printf("fizz");
+			}
+			if (x % 5 == 0)
+			{
+				printf("buzz");
+			}
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
