aae8d12eac11137a8324cc0fc714a58cf4df4a20
wafaetaouil <wafaetaouil@gmail.com>
2023-05-02 16:13:45 -0700

    ./4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i, j, l;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	l = i;
+
+	for (j = l - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
