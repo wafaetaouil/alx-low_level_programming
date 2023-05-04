04e3005189e3d25f675741328f4d91bac9ff2ca8
wafaetaouil <wafaetaouil@gmail.com>
2023-05-04 04:28:18 -0700

    ./3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - compare 2 strings.
+ * @s1: string to be compared.
+ * @s2: string for comparation.
+ * Return: Difference.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
