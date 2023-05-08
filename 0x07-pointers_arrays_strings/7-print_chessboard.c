66eb0373bbb6ff5b36b9707c05a00c363881fa28
wafaetaouil <wafaetaouil@gmail.com>
2023-05-08 07:28:25 -0700

    ./7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,9 +10,9 @@ void print_chessboard(char (*a)[8])
 {
 	int i, n;
 
-	for (i = 0; i < 0; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (n = 0; n < 0; n++)
+		for (n = 0; n < 8; n++)
 			_putchar(a[i][n]);
 		_putchar('\n');
 	}
