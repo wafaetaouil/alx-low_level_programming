b8a63df13d92ddbe4519cd7cd7b48b9027b64558
wafaetaouil <wafaetaouil@gmail.com>
2023-05-08 07:20:20 -0700

    ./7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - entrer point
+ * @a: array
+ *
+ * Return: always 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (n = 0; n < 0; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
