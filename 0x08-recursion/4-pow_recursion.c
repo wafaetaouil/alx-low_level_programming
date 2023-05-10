7f8eb091b891846c0b296c47b66ed375183d1265
wafaetaouil <wafaetaouil@gmail.com>
2023-05-10 03:21:32 -0700

    ./4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the value x
+ * @x: value to raise
+ * @y: power
+ *
+ * Return: result of power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
