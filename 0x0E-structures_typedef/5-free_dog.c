fbc4420d0a985d886cfb52fe1d7e14e51a0a1e83
wafaetaouil <wafaetaouil@gmail.com>
2023-05-22 07:21:41 -0700

    task6

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free memory
+ * @d: to free
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
