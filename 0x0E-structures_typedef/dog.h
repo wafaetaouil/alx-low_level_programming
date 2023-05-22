a6aef1aea2f1bc021d9461ee4b6809e6731f458a
wafaetaouil <wafaetaouil@gmail.com>
2023-05-22 05:43:43 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - adogs basic
+ * @name: f mumber
+ * @age: s mumber
+ * @owner: third mumber
+ *
+ * Description: longer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ *
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+#endif
